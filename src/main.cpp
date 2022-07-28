24da93674d74b08cac278c0eef52d4572ef76acc
hushengjie <hushengjie@corp.netease.com>
2022-07-28 18:32:27 +0800

    #0 添加shader，完成顶点着色器和片段着色器，并写入着色器类。

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <shader_s.h>
 
 void processInput(GLFWwindow* window)
 {
@@ -32,14 +33,50 @@ int main()
 
     glViewport(0, 0, 800, 600);
 
+    Shader ourShader("shaders/shader.vs", "shaders/shader.fs");
+
+    // 设置三角形
+    float vertices[] = {
+        // 位置              // 颜色
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+    };
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    // 1. 绑定VAO
+    glBindVertexArray(VAO);
+    // 2. 把顶点数组复制到缓冲中供OpenGL使用
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 3. 设置顶点属性指针
+    // 位置属性
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // 颜色属性
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
     while (!glfwWindowShouldClose(window))
     {
         // 输入
         processInput(window);
 
         // render
+        // 清除颜色缓冲
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // 激活着色器
+        ourShader.use();
+
+        // 绘制三角形
+        ourShader.setFloat("someUniform", 1.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // 检查并调用事件，交换缓冲
         glfwSwapBuffers(window);
